1f7d3610176a2cb961e5d03ed49dc3c74ffe2f7a
brody king <46272894+brodyking@users.noreply.github.com>
2022-01-17 08:46:09 +0000

    look in release for 3.0

--- kernal.js
+++ kernal.js
@@ -1,6 +1,8 @@
 //Make the DIV element draggagle:
-dragElement(document.getElementById("popup"));
-dragElement(document.getElementById("main"));
+
+function triggerMove(title) {
+dragElement(document.getElementById(title));
+}
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -67,51 +69,50 @@ function selectText(id){
 	}
 }
 
-function popup(state,title,body,width,height) {
-  if (state == "show") {
-    document.getElementById("popup").style.display = "block";
-  } else if (state == "hide") {
-    document.getElementById("popup").style.display = "none";
-  }
-  document.getElementById("popupTitle").innerHTML = title;
-  document.getElementById("popupBody").innerHTML = body;
-  document.getElementById("popup").style.width = width;
-  document.getElementById("popup").style.height = height;
+
+function popup(title,body,width,height) {
+  document.body.innerHTML += '<div style="background-color: white; border: 2px solid black; height: auto; position: absolute; width: auto;left: 30px;top:30px; border-top: 1px solid black;filter: drop-shadow(5px 5px 1px grey);resize: both;overflow: auto;display:block;" id="' + title + '"><div style="text-align: center;background-color: #557fa9; border: 1px solid black;" id="' + title + 'TitleBox" onmouseover="triggerMove(' + "'" + title + "'" + ')"><span class="title" style="background-color: #557fa9; color: white;font-weight: normal;float: left;"><a href="#" onclick="hidepopupLinks(' + "'" + title + "'" +')"><button style="border-bottom: 0px solid black;" id="hide' + title + 'Links">-</button></a></span><span class="title" style="background-color: #557fa9; color: white; display: inline;font-weight: normal;" id="' + title + 'Title">Empty Panel</span><span class="title" style="background-color: #557fa9; color: white;font-weight: normal;float: right;"><a href="#" onclick="hide(' + "'" + title + "'" + ')"><button style="border-bottom: 0px solid black;">x</button></a></span></div><div style="text-align: left; background-color: #c0c7c8; border: 1px solid black;border-bottom: 2px solid black;" id="' + title + 'Links"><a class="link" onclick="hide(' + "'" + title + "'" + ')" href="#" id="closeLink">Close</a></div><div id="' + title + 'Body"></div></div>'
+  document.getElementById(title + "Title").innerHTML = title;
+  document.getElementById(title + "Body").innerHTML = body;
+  document.getElementById(title).style.width = width;
+  document.getElementById(title).style.height = height;
+  dragElement(document.getElementById(title));
+  dragElement(document.getElementById("main"));
 }
 
-function main(title,body) {
-  document.getElementById("mainTitle").innerHTML = title;
-  document.getElementById("mainBody").innerHTML = body;
-}
+var shortcuts = "<li id='apps'><a href='#' onclick='manager();'>Manager</a></li><li><a href='#' onclick='install()'>Install</a></li><li><a href='#' onclick='clock()'>Clock</a></li><li><a href='#' onclick='about()'>About</a></li>"
 
-var shortcuts = "<li><a href='#' onclick='manager()'>Manager</a></li>"
+function about() {
+  popup("About","You are currently using ExOS v0.3<br>This version includes infinite popups and a new taskbar at the top!");
+}
 
 function manager() {
-  main('Manager','<ul style="margin: 0px; font-weight: bold; padding: 0px; list-style-type: none;">' + shortcuts + '</ul>')
+  document.getElementById("mainTitle").innerHTML = 'Manager';
+  document.getElementById("mainBody").innerHTML = '<ul style="margin: 0px; font-weight: bold; padding: 0px; list-style-type: none;">' + shortcuts + '</ul>';
 }
 
 manager()
 
 function addToManager(title,action) {
   shortcuts += "<li><a href='#' onclick='" + action + "'>" + title + "</a><li>"
-  main('Manager','<ul style="margin: 0px; font-weight: bold; padding: 0px;">' + shortcuts + '</ul>')
+  document.getElementById("mainTitle").innerHTML = 'Manager';
+  document.getElementById("mainBody").innerHTML = '<ul style="margin: 0px; font-weight: bold; padding: 0px;">' + shortcuts + '</ul>';
 }
 
 function finishinstalling() {
  var script = document.createElement('script');
  script.src = "https://brodyking.github.io/ExOS-Apps/" + document.getElementById("packagename").textContent + ".js";
  document.head.appendChild(script);
- popup('show','Install',document.getElementById("packagename").textContent + " was installed.<br><br><button onclick='closepopup()'>Close</a>")
+ var packagename = document.getElementById("packagename").textContent;
+ hide("Install");
+ popup('Install',packagename + " was installed.","232px","103px");
 }
 
 
 function install() {
-  popup('show','Install',"Package Name:<br>><span contenteditable='true' id='packagename' onclick='selectText(this.id)'>_</span><br><br><button onclick='finishinstalling()' style='margin-right: 10px;'>Install Script</buton><button onclick='installunsigned()'>Install Unsigned</a>","500px","auto")
+  popup('Install',"Package Name:<br>><span contenteditable='true' id='packagename' onclick='selectText(this.id)'>_</span><br><br><button onclick='finishinstalling()' style='margin-right: 10px;'>Install Script</buton>","500px","auto")
 }
 
-function closepopup() {
-  popup('hide')
-}
 
 var mainState = "1"
 
@@ -131,25 +132,36 @@ function hidemainLinks() {
 
 var popupState = "1"
 
-function hidepopupLinks() {
+function hidepopupLinks(name) {
   if (popupState == "1") {
-  document.getElementById("popupLinks").style.display = "none";
-  document.getElementById("popupTitleBox").style.borderBottom = "2px solid black";
-  document.getElementById("hidepopupLinks").innerHTML = "+";
+  document.getElementById(name + "Links").style.display = "none";
+  document.getElementById(name + "TitleBox").style.borderBottom = "2px solid black";
+  document.getElementById("hide" + name + "Links").innerHTML = "+";
   popupState = "0"
   } else if (popupState == "0") {
-  document.getElementById("popupLinks").style.display = "block";
-  document.getElementById("popupTitleBox").style.borderBottom = "1px solid black";
-  document.getElementById("hidepopupLinks").innerHTML = "-";
+  document.getElementById(name + "Links").style.display = "block";
+  document.getElementById(name + "TitleBox").style.borderBottom = "1px solid black";
+  document.getElementById("hide" + name + "Links").innerHTML = "-";
   popupState = "1"
   }
 }
 
-function installunsigned() {
-  popup('show','Install',"<span style='color: red'>NOTE: WE DO NOT RECOMMEND DOING THIS IF YOU DO NOT TURST THE AUTHOR</span><br>Package Source:<br>><span contenteditable='true' id='packagesource' onclick='selectText(this.id)'>_</span><br><br><button onclick='finishinstallingunsigned()' style='margin-right: 10px;'>Install Script</buton>","500px","auto")
+
+setInterval(function(){ 
+  var today = new Date();
+  var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' ] '+time;
+  document.getElementById("datetime").innerHTML = dateTime;  
+}, 100);
+
+var clockOpen = "0"
+
+function clock() {
+  clockOpen = "1"
+  popup("Clock","The current date/time is <i>" + document.getElementById("datetime").textContent + "</i><br>If you want a live update, look at the top right lol.","auto","79px");
 }
 
-function finishinstallingunsigned() {
- document.getElementById("unsigned").innerHTML = document.getElementById("packagesource").textContent;
- popup('show','Install',"a unsigned package was installed. (unsigned)<br><br><button onclick='closepopup()'>Close</a>");
+function hide(popup) {
+  document.getElementById(popup).remove();
 }
